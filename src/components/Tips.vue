f1ac2a8f52b88fbcb04d69c92dfae48c4047c95e
diandi <diandi@in66.com>
2017-06-09 17:48:47 +0800

    init

---
+++ src/components/Tips.vue
@@ -0,0 +1,17 @@
+<template>
+    <div v-if="show"></div>
+</template>
+<script>
+    export default {
+      data () {
+        return {
+          show: false
+        }
+      },
+      mounted () {
+    
+      }
+    }
+</script>
+<style lang="scss">
+</style>
